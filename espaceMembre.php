881db078b3a3ca71e0d1dfbdd2883db1f887bba8
tatane52 <jonathanderiot@gmail.com>
2017-06-27 16:14:45 +0200

    creation page espace membre avec sa page css

---
+++ espaceMembre.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>espace membre</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="membre.css">
+</head>
+
+<body>
+    <?php include('enTeteAnnexe.php'); ?>  
+</body>
+</html>
