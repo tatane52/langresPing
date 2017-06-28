70b0857edc1d9ceda81a1473a1781e199750a810
tatane52 <jonathanderiot@gmail.com>
2017-06-28 13:28:00 +0200

    formatage tchat

--- espaceMembre.php
+++ espaceMembre.php
@@ -9,6 +9,31 @@
 </head>
 
 <body>
-    <?php include('enTeteAnnexe.php'); ?>  
+    <?php include('enTeteAnnexe.php'); ?>
+    <main>
+        
+    
+    <section class="container" id="tchat">
+        <div class="row title col-xs-offset-2 col-xs-8 text-center">
+            <h2>Tchat du BB Langres</h2>
+        </div>
+        <div class="row talk col-xs-offset-2 col-xs-8">
+            <p>ksfiezfpozef,oez,foez,foez,foez,fez,fezf,ezfezi</p>
+            <p>ksfiezfpozef,oez,foez,foez,foez,fez,fezf,ezfezi</p>
+            <p>ksfiezfpozef,oez,foez,foez,foez,fez,fezf,ezfezi</p>
+            <p>ksfiezfpozef,oez,foez,foez,foez,fez,fezf,ezfezi</p>
+            <p>ksfiezfpozef,oez,foez,foez,foez,fez,fezf,ezfezi</p>
+            <p>ksfiezfpozef,oez,foez,foez,foez,fez,fezf,ezfezi</p>
+            <p>ksfiezfpozef,oez,foez,foez,foez,fez,fezf,ezfezi</p>
+        </div>
+        <div class="row send col-xs-offset-2 col-xs-8 text-center">
+            <form method="post" action="tchat.php">
+                <textarea name="message"></textarea></br>
+                <button type="submit">Envoyer</button>
+            </form>
+            
+        </div>
+    </div>
+    </main>
 </body>
 </html>
