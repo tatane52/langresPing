ce0ba8062c4cdb5167642d8b3d3f0edcc87efc10
tatane52 <jonathanderiot@gmail.com>
2017-07-04 16:49:56 +0200

    recuperation donnees et affichage dans espaceMembre

--- espaceMembre.php
+++ espaceMembre.php
@@ -14,7 +14,7 @@ session_start();
 <body>
 
     <?php include('enTeteAnnexe.php');
-    echo "bienvenue dans l'espace membre " .$_SESSION['pseudo'];
+    echo "<p class='text-center'>bienvenue dans l'espace membre " .$_SESSION['pseudo']. "!!!</p>";
     ?>
     <main>
         
@@ -24,17 +24,32 @@ session_start();
             <h2>Tchat du BB Langres</h2>
         </div>
         <div class="row talk col-xs-offset-2 col-xs-8">
-            
+            <?php
+                require_once('connectionBdd.php');
+                $requeteSelect = "SELECT * FROM tchat ORDER BY id_tchat DESC LIMIT 6";
+                $result = $bdd->query($requeteSelect);
+                while($donnees = $result->fetch()){
+                    $date_envoi = new Datetime($donnees['date_envoi']);
+                    $dateFormat = date_format($date_envoi, 'd-m-Y H:i:s');
+                    if($donnees['pseudo'] == $_SESSION['pseudo']){
+                        echo "<p><span class='pseudoMoi'>" .$donnees['pseudo']. "</span> : le " .$dateFormat. "</br>"
+                        .$donnees['message']."</p>";
+                    }
+                    else{
+                        echo "<p class='text-right'><span class='pseudoAutre'>" .$donnees['pseudo']. "</span> : le " .$dateFormat. "</br>"
+                        .$donnees['message']."</p>";
+                    }
+                }
+            ?>             
         </div>
         <div class="row send col-xs-offset-2 col-xs-8 text-center">
             <form method="post" action="tchat.php">
                 <textarea name="message"></textarea></br>
-                <button type="submit">Envoyer</button>
+                <button type="submit" name="send">Envoyer</button>
             </form>
             
         </div>
     </div>
     </main>
-
 </body>
 </html>
---
+++ tchat.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+if(isset($_POST['send'])){
+    require_once('connectionBdd.php');
+    $pseudo = $_SESSION['pseudo'];
+    $message = htmlentities($_POST['message']);
+    $date_envoi = date('Y-m-d H:i:s');
+    
+    $requeteInsert = "INSERT INTO tchat VALUES (null, '$message', '$pseudo', '$date_envoi')";
+    //echo $requeteInsert;
+
+    $bdd->exec($requeteInsert);
+    header('location: espaceMembre.php');
+}
+?>
