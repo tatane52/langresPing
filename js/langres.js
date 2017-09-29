b2ed2e58b7ba597f9660f403faa4f80a69132ee2
tatane52 <jonathanderiot@gmail.com>
2017-09-29 16:58:46 +0200

    ajout commentaires et supression duplication de code

--- js/langres.js
+++ js/langres.js
@@ -1,53 +1,51 @@
 $(document).ready(function() {
 
     $(".lienHisto").click(function(e) {
+        //récupération de la largeur de la fenêtre
         var larg = document.body.clientWidth;
+        //récupération du décalage entre la section et le haut de la page
+        var top = $("#historique").offset().top;
+        //annulation du positionnement par défaut
+        e.preventDefault();
         if (larg > 767) {
-            var top = $("#historique").offset().top;
-            e.preventDefault();
+            //mise en place de la page au bon endroit (position x, position y)
             window.scrollTo(0, top - 125);
         } else {
-            var top = $("#historique").offset().top;
-            e.preventDefault();
             window.scrollTo(0, top - 75);
         }
     });
 
     $(".lienEffect").click(function(e) {
         var larg = document.body.clientWidth;
+        console.log(larg);
+        var top = $("#effectif").offset().top;
+        console.log(top);
+        e.preventDefault();
         if (larg > 767) {
-            var top = $("#effectif").offset().top;
-            e.preventDefault();
             window.scrollTo(0, top - 125);
         } else {
-            var top = $("#effectif").offset().top;
-            e.preventDefault();
             window.scrollTo(0, top - 75);
         }
     });
 
     $(".lienCalend").click(function(e) {
         var larg = document.body.clientWidth;
+        var top = $("#calendrier").offset().top;
+        e.preventDefault();
         if (larg > 767) {
-            var top = $("#calendrier").offset().top;
-            e.preventDefault();
             window.scrollTo(0, top - 125);
         } else {
-            var top = $("#calendrier").offset().top;
-            e.preventDefault();
             window.scrollTo(0, top - 75);
         }
     });
 
     $(".lienCont").click(function(e) {
         var larg = document.body.clientWidth;
+        var top = $("#contact").offset().top;
+        e.preventDefault();
         if (larg > 767) {
-            var top = $("#contact").offset().top;
-            e.preventDefault();
             window.scrollTo(0, top - 125);
         } else {
-            var top = $("#contact").offset().top;
-            e.preventDefault();
             window.scrollTo(0, top - 75);
         }
     });
